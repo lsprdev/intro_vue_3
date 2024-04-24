b61bb0c423daf7774927eac4cdaa1da70e67b8b8
lsprgabriel <ogabrielpereira@pm.me>
2024-04-24 17:27:13 -0300

    atv 1

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            description: 'A pair of warm, fuzzy boots',
+        }
+    }
+})
